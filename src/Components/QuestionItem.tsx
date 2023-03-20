dedb9c7d6e24e8eda591d767a7e9c505a8764d9c
Guillaume Lamarche <guillaumelamarche1@gmail.com>
2023-03-20 14:47:15 -0400

    feat:added radio buttons to choices

--- src/Components/QuestionItem.tsx
+++ src/Components/QuestionItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IQuestionItem } from "../interfaces";
 
 interface questionItemProps {
@@ -5,16 +6,44 @@ interface questionItemProps {
 }
 
 export const QuestionItem = ({ question }: questionItemProps) => {
+  const [selectedChoice, setSelectedChoice] = useState<string | null>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (selectedChoice === question.answer) {
+      console.log("Correct!");
+    } else {
+      console.log("Incorrect!");
+    }
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedChoice(e.target.value);
+  };
+
   return (
     <>
-      <h3>{question.question}</h3>
-      {question.type === "MultipleChoice" && (
-        <ul>
-          {question.choices?.map((choice) => (
-            <li key={choice}>{choice}</li>
-          ))}
-        </ul>
-      )}
+      <form
+        onSubmit={handleSubmit}
+        className="m-5 rounded-md border-2 border-blue-800 p-5"
+      >
+        <h2 className="text-xl">{question.question}</h2>
+        {question.type === "MultipleChoice" && (
+          <ul>
+            {question.choices?.map((choice) => (
+              <div key={choice} className="flex justify-between">
+                <label>{choice}</label>
+                <input
+                  type="radio"
+                  name="choices"
+                  value={choice}
+                  onChange={handleChange}
+                ></input>
+              </div>
+            ))}
+          </ul>
+        )}
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 };
