64eebf1d018e127c289b62f4fc19bd50a3b13da0
Guillaume Lamarche <guillaumelamarche1@gmail.com>
2023-03-22 16:03:50 -0400

    feat: added parser functionality and tests

--- src/Utility/QuestionParser.ts
+++ src/Utility/QuestionParser.ts
@@ -1,11 +1,14 @@
 const questionKeywords = ["Question", "question", "Q", "q"];
 const answerKeywords = ["Answer", "answer", "A", "a"];
-const multipleChoiceKeywords = ["MultipleChoice", "multiplechoice", "MC", "mc"];
+const multipleChoiceKeywords = ["-"];
 
 export const parseBruteText = (text: string) => {
   const questions: string[] = [];
   const answers: string[] = [];
-  const lines = text.split(/\r?\n/);
+  const choices: string[] = [];
+
+  let currentQuestionId = 0;
+  const lines = splitTextIntoLines(text);
 
   lines.forEach((line) => {
     if (isLineQuestion(line)) {
@@ -14,27 +17,60 @@ export const parseBruteText = (text: string) => {
     } else if (isLineAnswer(line)) {
       line = formatAnswer(line);
       answers.push(line);
+      currentQuestionId++;
+    } else if (isLineMultipleChoice(line)) {
+      line = formatMultipleChoice(line);
+      choices.push(line);
     }
   });
-  console.log(questions, answers);
-  return questions;
+
+  console.log("Questions: ", questions);
+  console.log("Answers: ", answers);
+  console.log("Choices: ", choices);
+
+  return { questions, answers, choices };
+};
+
+const splitTextIntoLines = (text: string) => {
+  const lines = text.split(/\r?\n/);
+  return lines;
+};
+
+export const splitTextIntoQuestionBlocks = (text: string): string[] => {
+  var blocks: string[] = [];
+  var currentBlock = "";
+  const lines = splitTextIntoLines(text);
+  lines.forEach((line) => {
+    if (!isLineAnswer(line)) {
+      currentBlock += line + "\n";
+    }
+    if (isLineAnswer(line)) {
+      currentBlock += line;
+      currentBlock = currentBlock.trim();
+      blocks.push(currentBlock);
+      currentBlock = "";
+    }
+  });
+  return blocks;
 };
 
 const isLineQuestion = (line: string) => {
   //check if line contains any of the keywords
   const containsKeyword = questionKeywords.some((keyword) =>
-    line.includes(keyword)
+    line.includes(keyword + ":")
   );
 
   return containsKeyword && line.includes("?");
 };
+
 const isLineAnswer = (line: string) => {
   //check if line contains any of the keywords
   const containsKeyword = answerKeywords.some((keyword) =>
-    line.includes(keyword)
+    line.includes(keyword + ":")
   );
   return containsKeyword;
 };
+
 const isLineMultipleChoice = (line: string) => {
   //check if line contains any of the keywords
   const containsKeyword = multipleChoiceKeywords.some((keyword) =>
@@ -44,33 +80,23 @@ const isLineMultipleChoice = (line: string) => {
 };
 
 const formatQuestion = (question: string) => {
-  //remove keywords
-  questionKeywords.forEach((keyword) => {
-    question = question.replace(keyword, "");
-  });
-
-  //remove :
-  question = question.replace(":", "");
   // remove whitespace at beginning and end
   question = question.trim();
 
   return question;
 };
+
 const formatAnswer = (answer: string) => {
-  //remove keywords
-  answerKeywords.forEach((keyword) => {
-    answer = answer.replace(keyword, "");
-  });
-  //remove :
-  answer = answer.replace(":", "");
-  // remove whitespace at beginning and end
   answer = answer.trim();
   return answer;
 };
-const formatMultipleChoice = (choices: string[]) => {
+
+const formatMultipleChoice = (choice: string) => {
   //remove keywords
-  multipleChoiceKeywords.forEach((keyword) => {
-    choices = choices.map((choice) => choice.replace(keyword, ""));
-  });
-  return choices;
+  let match = choice.match(/(MultipleChoice|multiplechoice|MC|mc|-):/);
+  if (match) {
+    choice = choice.replace(match[0], "");
+  }
+  choice = choice.trim();
+  return choice;
 };
--- src/__tests__/QuestionParser.test.js
+++ src/__tests__/QuestionParser.test.js
@@ -1,11 +1,66 @@
-import { parseBruteText } from "../Utility/QuestionParser";
+import {
+  parseBruteText,
+  splitTextIntoQuestionBlocks,
+} from "../Utility/QuestionParser";
 
 describe("QuestionParser", () => {
-  it("should parse a question", () => {
+  it("should parse a question with any keywords", () => {
+    const questionKeywords = ["Question", "question", "Q", "q"];
+    const answerKeywords = ["Answer", "answer", "A", "a"];
+
+    let questions = [];
+
+    questionKeywords.forEach((questionKeyword, index) => {
+      questions.push(
+        parseBruteText(
+          `${questionKeywords[index]}: Why is the sky blue?\n${answerKeywords[index]}: Because it is.`
+        )
+      );
+    });
+
+    questions.forEach((question, index) => {
+      let expectedQuestion = questionKeywords[index] + ": Why is the sky blue?";
+      let expectedAnswer = answerKeywords[index] + ": Because it is.";
+      expect(question).toEqual({
+        questions: [expectedQuestion],
+        answers: [expectedAnswer],
+        choices: [],
+      });
+    });
+  });
+
+  it("should parse multiple questions", () => {
     const question = parseBruteText(
-      "q: Why is the sky blue? \n a: Because it is."
+      " q: Why is the sky blue? \n a: Because it is. \n q: Why is the grass green? \n a: Because it is."
     );
 
-    expect(question).toEqual(["Why is the sky blue?"]);
+    expect(question).toEqual({
+      questions: ["q: Why is the sky blue?", "q: Why is the grass green?"],
+      answers: ["a: Because it is.", "a: Because it is."],
+      choices: [],
+    });
+  });
+
+  it("should parse a multiple choice question", () => {
+    const question = parseBruteText(
+      "q: What is the capital of France?\n -Paris\n -London\n -Rome\n a: Paris"
+    );
+
+    expect(question).toEqual({
+      questions: ["q: What is the capital of France?"],
+      answers: ["a: Paris"],
+      choices: ["-Paris", "-London", "-Rome"],
+    });
+  });
+  it("should split text into question blocks", () => {
+    const text = `q: Why is the sky blue? \n a: Because it is. \n q: Why is the grass green? \n a: Because it is. \n q: What is the capital of France? \n -Paris \n -London \n -Rome \n a: Paris`;
+
+    const questionBlocks = splitTextIntoQuestionBlocks(text);
+
+    expect(questionBlocks).toEqual([
+      "q: Why is the sky blue? \n a: Because it is.",
+      "q: Why is the grass green? \n a: Because it is.",
+      "q: What is the capital of France? \n -Paris \n -London \n -Rome \n a: Paris",
+    ]);
   });
 });
