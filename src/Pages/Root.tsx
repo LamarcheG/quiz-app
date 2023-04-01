3696242a07be01e50e6b1c10cad26ea7ee3b5155
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-01 11:36:40 -0400

    fix:disabling login and signup routes

--- src/Pages/Root.tsx
+++ src/Pages/Root.tsx
@@ -6,8 +6,8 @@ export const Root = () => {
     <div>
       <nav className="absolute top-0 left-0 flex w-6/12 items-center justify-between">
         <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Signup</Link>
+        {/* <Link to="/login">Login</Link>
+        <Link to="/signup">Signup</Link> */}
         <Link to="/stacks">My stacks</Link>
         <Link to="/quiz">Temp</Link>
       </nav>
