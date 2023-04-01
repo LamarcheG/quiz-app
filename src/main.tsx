a3a12632567d3ec3382de263cfa259e048298105
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-01 11:13:03 -0400

    feat:added react-router-dom to project

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { QuestionStackProvider } from "./Stores/QuestionStackContext";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ErrorPage } from "./Pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QuestionStackProvider>
-      <App />
+      <RouterProvider router={router} />
     </QuestionStackProvider>
   </React.StrictMode>
 );
