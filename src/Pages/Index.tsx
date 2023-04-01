bd18e3d9aa0c2aff479d9ddb22edec586f78545a
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-01 11:25:42 -0400

    feat: added basic nav element

---
+++ src/Pages/Index.tsx
@@ -0,0 +1,8 @@
+export const Index = () => {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>Welcome to the best Quiz app of Paradis street</p>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
 import { QuestionStackProvider } from "./Stores/QuestionStackContext";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ErrorPage } from "./Pages/ErrorPage";
+import { Root } from "./Pages/Root";
+import App from "./App";
+import { Index } from "./Pages/Index";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Index /> },
+      {
+        path: "/quiz",
+        element: <App />,
+      },
+    ],
   },
 ]);
 
