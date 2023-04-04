cadf8ef4b3549b8a7a2bd4ee026b2513675a6160
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-04 19:14:44 -0400

    style: changed nav to be on the side

--- src/Pages/Root.tsx
+++ src/Pages/Root.tsx
@@ -3,9 +3,11 @@ import { Link, Outlet } from "react-router-dom";
 export const Root = () => {
   return (
     <div className="mx-auto w-fit">
-      <nav className="absolute top-0 left-0 flex w-3/12 items-center justify-between">
-        <Link to="/">Home</Link>
-        <Link to="/stacks">My stacks</Link>
+      <nav className="absolute top-0 left-0 flex h-full w-32 flex-col items-center justify-between bg-gray-700">
+        <div className="flex h-24 flex-col items-center justify-between">
+          <Link to="/">Home</Link>
+          <Link to="/stacks">My stacks</Link>
+        </div>
       </nav>
       <Outlet />
     </div>
