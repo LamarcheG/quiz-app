7b93f2ac9ab565616d890bbaf4dc6c58af93b1a9
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-10 10:11:27 -0400

    style: made next and prev question buttons smaller

--- src/Components/Questions/QuestionList.tsx
+++ src/Components/Questions/QuestionList.tsx
@@ -78,7 +78,7 @@ export const QuestionList = ({ questions }: QuestionListProps) => {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 320 512"
-          className="h-16 w-16"
+          className="h-10 w-10"
         >
           <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
         </svg>
@@ -122,7 +122,7 @@ export const QuestionList = ({ questions }: QuestionListProps) => {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 320 512"
-          className="h-16 w-16"
+          className="h-10 w-10"
         >
           <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
         </svg>
