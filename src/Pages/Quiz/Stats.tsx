a808bafd999cbcec2aa7ba5a0ee32aab9e2fcb9e
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-11 11:35:19 -0400

    feat: avg score and time for stack

--- src/Pages/Quiz/Stats.tsx
+++ src/Pages/Quiz/Stats.tsx
@@ -1,4 +1,10 @@
-import { doc, getDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  onSnapshot,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import db from "../../firebaseInit";
@@ -7,29 +13,89 @@ import { useUser } from "../../Stores/UserContext";
 
 export const Stats = () => {
   const { stackId } = useParams();
-  const [stackName, setStackName] = useState("");
+  const [statListOriginal, setStatListOriginal] = useState<any[]>([]);
+  const [statList, setStatList] = useState<any[]>([]);
   const userContext = useUser() as unknown as User;
 
+  const convertFirebaseDate = (date: any) => {
+    const newDate = new Date(date.seconds * 1000);
+    return newDate;
+  };
+
   useEffect(() => {
-    const getStackName = async () => {
-      const stackRef = doc(
+    const getStackStats = async () => {
+      const stackRef = collection(
         db,
-        `/users/${userContext.user.uid}/stacks/${stackId}`
+        `/users/${userContext.user.uid}/stacks/${stackId}/stats`
       );
-      const stackDoc = await getDoc(stackRef);
-      if (stackDoc.exists()) {
-        setStackName(stackDoc.data().name);
-      } else {
-        console.log("No such document!");
-      }
+      onSnapshot(stackRef, (snapshot) => {
+        const stats = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setStatListOriginal(stats);
+      });
     };
-    getStackName();
+    getStackStats();
   }, []);
+  useEffect(() => {
+    const newStatList = statListOriginal.map((stat) => {
+      stat.date = convertFirebaseDate(stat.date);
+      return stat;
+    });
+    sortStats(newStatList);
+    setStatList(newStatList);
+  }, [statListOriginal]);
+
+  const sortStats = (statList: any[]) => {
+    statList.sort((a, b) => {
+      return a.date - b.date;
+    });
+  };
+
+  const formatDateTime = (date: Date) => {
+    const newDate = new Date(date);
+    return (
+      newDate.toDateString() +
+      " " +
+      newDate.getHours() +
+      ":" +
+      newDate.getMinutes()
+    );
+  };
+
+  const getAverageScore = () => {
+    let total = 0;
+    statList.forEach((stat) => {
+      total += stat.score;
+    });
+    let avg = Math.round((total / statList.length) * 100) / 100;
+    return avg.toString() + "%";
+  };
+
+  const getAverageTime = () => {
+    let total = 0;
+    statList.forEach((stat) => {
+      total += stat.time;
+    });
+    return (total / statList.length).toFixed(2) + " seconds";
+  };
+
   return (
     <div>
-      <h1>Stats</h1>
-      <p>Stack ID: {stackId}</p>
-      <p>Stack Name: {stackName}</p>
+      {statList.map((stat) => {
+        return (
+          <div key={stat.id} className="border">
+            <p>{formatDateTime(stat.date)}</p>
+            <p>{stat.score}</p>
+            <p>{stat.time}</p>
+          </div>
+        );
+      })}
+      <div>
+        <p>Average Score: {getAverageScore()}</p>
+        <p>Average Time: {getAverageTime()}</p>
+      </div>
     </div>
   );
 };
