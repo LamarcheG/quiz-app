09de39161046176c662a58a6d276dd31e32054ee
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-12 15:10:50 -0400

    style: changed width of nav links

--- src/Components/Navigation/Nav.tsx
+++ src/Components/Navigation/Nav.tsx
@@ -44,7 +44,7 @@ export const Nav = () => {
                   <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" />
                 </svg>
               </button>
-              <div className="m-auto flex min-h-min w-2/3 flex-col items-start justify-center">
+              <div className="m-auto flex min-h-min w-fit flex-col items-start justify-center">
                 <div className="flex flex-row pt-5">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -88,7 +88,7 @@ export const Nav = () => {
         </>
       ) : (
         <nav className="min-h-screen w-44 flex-shrink-0 bg-gray-900 2xl:w-64">
-          <div className="m-auto flex min-h-min w-2/3 flex-col items-start justify-center">
+          <div className="m-auto flex min-h-min w-fit flex-col items-start justify-center">
             <div className="flex flex-row pt-5">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
