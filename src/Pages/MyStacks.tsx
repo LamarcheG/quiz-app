641f9fb27b0455f9e765fa7070a5be4f11ebccc6
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-18 17:28:36 -0400

    feat: added stats to Mystacks page

--- src/Pages/MyStacks.tsx
+++ src/Pages/MyStacks.tsx
@@ -1,4 +1,4 @@
-import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import { addDoc, collection, getDocs, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import db from "../firebaseInit";
 import { Link } from "react-router-dom";
@@ -11,6 +11,7 @@ export const MyStacks = (props: any) => {
   const [displayForm, setDisplayForm] = useState(false);
   const [newSubject, setNewSubject] = useState("");
   const userContext = useUser() as unknown as User;
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const subscribeToStacks = () => {
     const collectionRef = collection(
@@ -20,16 +21,52 @@ export const MyStacks = (props: any) => {
     const unsubscribe = onSnapshot(
       collectionRef,
       (snapshot) => {
-        const stacksArray = snapshot.docs.map((doc) => {
-          return { ...doc.data(), id: doc.id };
+        const stacksArray = snapshot.docs.map(async (doc) => {
+          const stats = await getStats(doc.id);
+          return { ...doc.data(), id: doc.id, stats };
         });
-        setStacks(stacksArray);
+        Promise.all(stacksArray).then((values) => {
+          setStacks(values);
+          setIsLoaded(true);
+        });
       },
       (error) => {
         console.log(error);
       }
     );
     return unsubscribe;
+  };
+
+  const getStats = async (id: string) => {
+    const collectionRef = collection(
+      db,
+      `/users/${userContext.user.uid}/stacks/${id}/stats`
+    );
+
+    const statsList = await getDocs(collectionRef).then((snapshot) => {
+      const statsArray = snapshot.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+      });
+      return statsArray;
+    });
+    let processedStats = {};
+    if (statsList.length > 0) {
+      processedStats = processStats(statsList);
+    }
+    return processedStats;
+  };
+
+  const processStats = (stats: any[]) => {
+    let averageTime = 0;
+    let averageScore = 0;
+
+    stats.forEach((stat) => {
+      averageTime += stat.time;
+      averageScore += stat.score;
+    });
+    averageTime = Math.round((averageTime / stats.length) * 100) / 100;
+    averageScore = Math.round((averageScore / stats.length) * 100) / 100;
+    return { nbOfStats: stats.length, averageTime, averageScore };
   };
 
   useEffect(() => {
@@ -67,15 +104,44 @@ export const MyStacks = (props: any) => {
             </button>
           )}
         </div>
-        <ul className="items-left flex flex-col justify-center ">
-          {stacks.map((stack) => {
-            return (
-              <li key={stack.id} className="w-fit list-inside list-disc p-2">
-                <Link to={`/stacks/${stack.id}/quiz`}>{stack.name}</Link>
-              </li>
-            );
-          })}
-        </ul>
+        {!isLoaded ? (
+          <p>Loading...</p>
+        ) : (
+          <ul className="grid grid-cols-3 gap-3">
+            {stacks.map((stack) => {
+              return (
+                <li
+                  key={stack.id}
+                  className=" w-full rounded-md  bg-gray-900 p-5 shadow-md shadow-neutral-900 "
+                >
+                  <Link
+                    to={`/stacks/${stack.id}/quiz`}
+                    className="text-white hover:text-white"
+                  >
+                    <h2 className="mb-1 text-2xl font-bold text-neutral-400">
+                      {stack.name}
+                    </h2>
+                    {Object.keys(stack.stats).length > 0 ? (
+                      <div className="flex flex-col">
+                        <span>
+                          Completed: {stack.stats.nbOfStats}{" "}
+                          {stack.stats.nbOfStats > 0 ? "times" : "time"}
+                        </span>
+                        <span>
+                          Average time: {stack.stats.averageTime} seconds
+                        </span>
+                        <span>Average score: {stack.stats.averageScore}%</span>
+                      </div>
+                    ) : (
+                      <span>Not started yet</span>
+                    )}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+
         {displayForm && (
           <form
             onSubmit={(e) => {
