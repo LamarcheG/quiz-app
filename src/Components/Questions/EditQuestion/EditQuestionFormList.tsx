b505a98bc08b37a0066ef3aa8ee58f66c895aac7
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-19 10:51:53 -0400

    fix: fix bug in edit page when no questions

--- src/Components/Questions/EditQuestion/EditQuestionFormList.tsx
+++ src/Components/Questions/EditQuestion/EditQuestionFormList.tsx
@@ -18,29 +18,37 @@ export const EditQuestionFormList = ({
   updateQuestion,
 }: EditQuestionFormListProps) => {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <button type="button" onClick={prevQuestion} className="mr-3 p-0">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 320 512"
-          className="h-10 w-10"
-        >
-          <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
-        </svg>
-      </button>
-      <EditQuestionForm
-        question={currentQuestion}
-        updateQuestion={updateQuestion}
-      />
-      <button type="button" onClick={nextQuestion} className="ml-3 p-0">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 320 512"
-          className="h-10 w-10"
-        >
-          <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
-        </svg>
-      </button>
-    </div>
+    <>
+      {currentQuestion ? (
+        <div className="flex h-full w-full items-center justify-center">
+          <button type="button" onClick={prevQuestion} className="mr-3 p-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+              className="h-10 w-10"
+            >
+              <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+            </svg>
+          </button>
+          <EditQuestionForm
+            question={currentQuestion}
+            updateQuestion={updateQuestion}
+          />
+          <button type="button" onClick={nextQuestion} className="ml-3 p-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+              className="h-10 w-10"
+            >
+              <path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" />
+            </svg>
+          </button>
+        </div>
+      ) : (
+        <div className="flex h-full w-full items-center justify-center">
+          <p className="text-white">No questions</p>
+        </div>
+      )}
+    </>
   );
 };
