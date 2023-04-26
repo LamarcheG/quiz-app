fec29595be26c63137433b358f782401c5c0f42a
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-26 09:51:52 -0400

    style: establish theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          dark: "#01335E",
+          DEFAULT: "#00539CFF",
+          light: "#027EEB",
+        },
+        secondary: {
+          dark: "#AD775C",
+          DEFAULT: "#EEA47FFF",
+          light: "#F0D5C7",
+        },
+        text: {
+          OverBlue: "#01539D",
+          OverPeach: "#EEA47F",
+          DEFAULT: "#e0e0e0",
+        },
+      },
+    },
   },
   plugins: [],
 };
