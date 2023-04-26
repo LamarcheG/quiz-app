4c9d16bd5db0ad4527f7400cab8f82d2157e0dde
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-26 19:17:14 -0400

    style: MyStacks card style

--- src/Pages/MyStacks.tsx
+++ src/Pages/MyStacks.tsx
@@ -82,15 +82,17 @@ export const MyStacks = (props: any) => {
                 <Link key={stack.id} to={`/stacks/${stack.id}/quiz`}>
                   <li className="relative h-full">
                     <div
-                      className="h-36 w-64 rounded-md border-t border-l border-gray-700 bg-gray-900 p-5 text-white shadow-md shadow-neutral-900 hover:text-white"
+                      className="h-36 w-64 rounded-md border-t border-l border-neutral-600 bg-neutral-800 text-white shadow-md shadow-black hover:text-white"
                       onMouseEnter={() => handleHover(true, stack.id)}
                       onMouseLeave={() => handleHover(false, stack.id)}
                     >
-                      <h2 className="mb-1 text-2xl font-bold text-neutral-400">
-                        {stack.name}
+                      <h2 className="grid h-1/3 items-center rounded-t-md border-b-2 border-primary">
+                        <span className="px-5 text-2xl font-bold text-text-OverBlue">
+                          {stack.name}
+                        </span>
                       </h2>
                       {Object.keys(stack.stats).length > 0 ? (
-                        <div className="flex flex-col">
+                        <div className="flex h-2/3 flex-col justify-center px-5">
                           <span>
                             Completed: {stack.stats.nbOfStats}{" "}
                             {stack.stats.nbOfStats > 1 ? "times" : "time"}
@@ -103,23 +105,29 @@ export const MyStacks = (props: any) => {
                           </span>
                         </div>
                       ) : (
-                        <span>Not started yet</span>
+                        <span className="flex h-2/3 items-center justify-center px-5">
+                          Not started yet
+                        </span>
                       )}
                     </div>
                     <div
                       className={
                         shouldHover(stack.id)
-                          ? "absolute top-0 left-0 -z-10 h-36 w-64 translate-x-8 -translate-y-4 rotate-6 rounded-md border-t border-l border-gray-700 bg-gray-900 shadow-md shadow-neutral-900 transition-all ease-in-out"
-                          : "absolute top-0 left-0 -z-10 h-36 w-64 rounded-md border-t border-l border-gray-700 bg-gray-900 transition-all ease-in-out"
+                          ? "absolute top-0 left-0 -z-10 h-36 w-64 translate-x-8 -translate-y-4 rotate-6 rounded-md border-t border-l border-neutral-600 bg-neutral-800 shadow-md shadow-black transition-all ease-in-out"
+                          : "absolute top-0 left-0 -z-10 h-36 w-64 rounded-md border-t border-l border-neutral-600 bg-neutral-800 transition-all ease-in-out"
                       }
-                    ></div>
+                    >
+                      <div className="h-1/3 border-b-2 border-primary"></div>
+                    </div>
                     <div
                       className={
                         shouldHover(stack.id)
-                          ? "absolute top-0 left-0 -z-10 h-36 w-64 translate-x-4 -translate-y-2 rotate-3 rounded-md border-t border-l border-gray-700 bg-gray-900 shadow-md shadow-neutral-900 transition-all ease-in-out"
-                          : "absolute top-0 left-0 -z-10 h-36 w-64 rounded-md border-t border-l border-gray-700 bg-gray-900 transition-all ease-in-out"
+                          ? "absolute top-0 left-0 -z-10 h-36 w-64 translate-x-4 -translate-y-2 rotate-3 rounded-md border-t border-l border-neutral-600 bg-neutral-800 shadow-md shadow-black transition-all ease-in-out"
+                          : "absolute top-0 left-0 -z-10 h-36 w-64 rounded-md border-t border-l border-neutral-600 bg-neutral-800 transition-all ease-in-out"
                       }
-                    ></div>
+                    >
+                      <div className="h-1/3 border-b-2 border-primary"></div>
+                    </div>
                   </li>
                 </Link>
               );
