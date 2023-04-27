4c9aadf89b4129937acd6024650155552dde3d9b
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-27 10:20:57 -0400

    style: created button click anim

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,16 @@ module.exports = {
           DEFAULT: "#e0e0e0",
         },
       },
+      keyframes: {
+        buttonClick: {
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(0.9)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
+      animation: {
+        buttonClick: "buttonClick 0.15s ease-in-out",
+      },
     },
   },
   plugins: [],
