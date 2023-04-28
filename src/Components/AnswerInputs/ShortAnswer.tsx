f4c324bce9fd302902abbc4ebc591af98e2f555c
LamarcheG <90644865+LamarcheG@users.noreply.github.com>
2023-04-28 15:32:09 -0400

    Style: change AnswerInput style

--- src/Components/AnswerInputs/ShortAnswer.tsx
+++ src/Components/AnswerInputs/ShortAnswer.tsx
@@ -9,7 +9,7 @@ export const ShortAnswer = ({ question, handleChange }: shortAnswerProps) => {
     <div className="flex items-center justify-between p-5">
       <label className="pr-3">Answer</label>
       <input
-        className="h-6 w-32 rounded-sm px-1 py-3"
+        className="h-6 w-32 rounded-md px-1 py-3 text-text-OverBlue"
         type="text"
         name={question.id}
         onChange={handleChange}
